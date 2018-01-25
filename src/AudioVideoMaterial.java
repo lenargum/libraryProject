e31206890f5b04b96d1f5e1540933d606e1612ed
Madina Gafarova <34398145+gafmn@users.noreply.github.com>
2018-01-25 17:24:12 +0300

    Edit Documents class

--- src/AudioVideoMaterial.java
+++ src/AudioVideoMaterial.java
@@ -5,8 +5,22 @@ public class AudioVideoMaterial extends Document{
 
     public Date publishingDate;
 
-    public AudioVideoMaterial(){ // constructor
-	
+    public AudioVideoMaterial(String title, ArrayList<String> authors, boolean is_reference, float price, int id, Date publDate){
+        setTitle(title);
+        setAuthors(authors);
+        setPrice(price);
+        setID(id);
+        this.reference = is_reference;
+        this.checked = false;
+        this.User = null;
+    }
+
+    public void setPublishingDate(Date publDate){
+        this.publishingDate = publDate;
+    }
+
+    public Date getPublishingDate(){
+        return publishingDate;
     }
 
 }
--- src/JournalArticle.java
+++ src/JournalArticle.java
@@ -7,9 +7,29 @@ public class JournalArticle extends Document{
     Date publishingDate;
     String journalPublisher;
 
-    public JournalArticle(){ //constructor
-
+    public JournalArticle(String title, ArrayList<String> authors, boolean is_reference, float price, int id, String jourTitle, Date publDate, String publisher){
+        setTitle(title);
+        setAuthors(authors);
+        setPrice(price);
+        setID(id);
+        setJournalPublisher(publisher);
+        setJournalTitle(jourTitle);
+        setPublishingDate(publDate);
+        this.reference = is_reference;
+        this.checked = false;
+        this.User = null;
     }
 
+    public void setJournalTitle(String jourTitle){
+        this.journalTitle = jourTitle;
+    }
+
+    public void setPublishingDate(Date publDate){
+        this.publishingDate = publDate;
+    }
+
+    public void setJournalPublisher(String publisher){
+        this.journalPublisher = publisher;
+    }
     //setters&getters&?
 }
