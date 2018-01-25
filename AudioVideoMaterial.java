df67ae80b06c5161008c5289352cdf14f6602867
stalem9 <35533350+stalem9@users.noreply.github.com>
2018-01-25 17:56:59 +0300

    Add files via upload

---
+++ AudioVideoMaterial.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AudioVideoMaterial extends Document{
+
+    public Date publishingDate;
+
+    public AudioVideoMaterial(String title, ArrayList<String> authors, int id){
+        super(authors, title,  id);
+    }
+
+    public void setPublishingDate(Date publDate){
+        this.publishingDate = publDate;
+    }
+
+    public Date getPublishingDate(){
+        return publishingDate;
+    }
+
+    //setters&getters&?
+}
