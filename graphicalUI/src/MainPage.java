da0002cf810664c022df20460d0d00894673c608
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-01-31 01:21:36 +0300

    UI updates
    + Added new login window
    + Login data now can be stored in runtime
    + Some login logic and improvements

--- graphicalUI/src/MainPage.java
+++ graphicalUI/src/MainPage.java
@@ -1,12 +1,17 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.effect.GaussianBlur;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainPage extends Application {
+
+	private char[] login, password;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -14,23 +19,66 @@ public class MainPage extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent welcome = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+		AnchorPane welcome = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
 		primaryStage.setTitle("InnoLibrary Manager");
-		primaryStage.setMinWidth(1024);
-		primaryStage.setMinHeight(768);
-		primaryStage.setScene(new Scene(welcome, 1024, 768));
+		primaryStage.setMinWidth(500);
+		primaryStage.setMinHeight(600);
+		Scene welcomeScene = new Scene(welcome, 1024, 768);
+		primaryStage.setScene(welcomeScene);
 		primaryStage.show();
 
 		Button loginButton = (Button) welcome.lookup("#loginButton");
 
-		Stage dialogStage = new Stage();
-		dialogStage.initOwner(primaryStage);
+//		Stage dialogStage = new Stage();
+//		dialogStage.setResizable(false);
+//		dialogStage.initOwner(primaryStage);
 
-		Parent loginScene = FXMLLoader.load(getClass().getResource("loginDialog.fxml"));
+		AnchorPane loginLayout = FXMLLoader.load(getClass().getResource("loginDialog.fxml"));
+		Scene loginScene = new Scene(loginLayout);
 		loginButton.setOnAction(event -> {
-			welcome.setEffect(new GaussianBlur());
-			dialogStage.setScene(new Scene(loginScene));
-			dialogStage.show();
+			switchScene(primaryStage, loginScene);
+
+//			dialogStage.setScene(new Scene(loginScene));
+//			dialogStage.show();
 		});
+
+		Button goBackButton = (Button) loginLayout.lookup("#goBackButton");
+		goBackButton.setOnAction(event -> {
+			PasswordField passField = (PasswordField) loginLayout.lookup("#passField");
+			passField.clear();
+			switchScene(primaryStage, welcomeScene);
+		});
+
+		Button completeLoginButton = (Button) loginLayout.lookup("#loginButton");
+		completeLoginButton.setOnAction(event -> {
+			TextField loginField = (TextField) loginLayout.lookup("#loginField");
+			PasswordField passField = (PasswordField) loginLayout.lookup("#passField");
+
+			if (loginField.getLength() > 0 && passField.getLength() > 0) {
+				login = loginField.getText().toCharArray();
+				password = passField.getText().toCharArray();
+
+				passField.clear();
+				// TODO Remove login button after successful authorization
+				//removeFromParent(loginLayout, loginButton);
+				switchScene(primaryStage, welcomeScene);
+			} else {
+				if (loginField.getLength() < 1) {
+					loginField.setStyle("-fx-border-color: RED");
+				}
+				if (passField.getLength() < 1) {
+					passField.setStyle("-fx-border-color: RED");
+				}
+			}
+		});
+	}
+
+	private void switchScene(Stage targetStage, Scene newScene) {
+		targetStage.setScene(newScene);
+		targetStage.show();
+	}
+
+	private void removeFromParent(Pane parent, Node toRemove) {
+		parent.getChildren().remove(toRemove);
 	}
 }
