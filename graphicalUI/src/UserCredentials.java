a918ee13e53ddacb8cd4d06fc94356e754506469
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-02-01 19:09:25 +0300

    UI updates
    - Some refactoring
    - "Login" button now disappears after successful login
    - User credentials now stored in separate class

---
+++ graphicalUI/src/UserCredentials.java
@@ -0,0 +1,22 @@
+public class UserCredentials {
+	private char[] login;
+	private char[] password;
+
+	public UserCredentials(String login, String password) {
+		this.login = login.toCharArray();
+		this.password = password.toCharArray();
+	}
+
+	public char[] getLogin() {
+		return login;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+
+	public boolean isAuthorized(){
+		//TODO Implement isAuthorized
+		return true;
+	}
+}
