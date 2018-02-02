c5eabff88689bebe6893f15d8e0607eaf1a4ea82
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-02-02 16:05:32 +0300

    Code reformat and cleanup, imports optimization

---
+++ src/AudioVideoMaterial.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+/**
+ * class implements "Audio and Video materials" type of Document
+ */
+public class AudioVideoMaterial extends Document {
+
+	private Date publishingDate;
+
+	public AudioVideoMaterial(String title, String authors, int id, float price, boolean isAllowedForStudents) {
+		super(authors, title, id, isAllowedForStudents, price);
+	}
+
+	public void setPublishingDate(Date publDate) {
+		this.publishingDate = publDate;
+	}
+
+	public Date getPublishingDate() {
+		return publishingDate;
+	}
+
+}
