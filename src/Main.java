3e9e6bf409a509e81973afe7ff91849f3c759732
Lenar <lenargamgam@gmail.com>
2018-02-02 18:41:43 +0300

    database insertion example

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Database db = new Database();
         db.connect();
+
     }
 }
+/*
+db.execute("INSERT INTO users(login, password,status,firstname,lastname,phone,address)" +
+                " VALUES('lenargumerov','1234','admin','lenar','gumerov','2281337','unversitetskaya')");
+ */
