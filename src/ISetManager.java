7a3bdc96f358bb9ad058f6219571aa93d8a09b65
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-02-02 16:05:32 +0300

    Code reformat and cleanup, imports optimization

---
+++ src/ISetManager.java
@@ -0,0 +1,11 @@
+public interface ISetManager {
+
+	public void addPatron(Patron patron);
+
+	public void deletePatron(int idPatron);
+
+	public void addDocumentInLibrary(Document document);
+
+	public void deleteDocumentFromLibrary(int idDocument);
+
+}
