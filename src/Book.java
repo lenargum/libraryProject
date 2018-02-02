1ea2e2cacc2e1887f06e657dc3f1f1569f012141
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-02-02 16:05:32 +0300

    Code reformat and cleanup, imports optimization

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book extends Document {
+	String Publisher;
+	int editionYear;
+
+	public Book(String title, String authors, int id, boolean allowed, float price) {
+		super(authors, title, id, allowed, price);
+	}
+
+	public void setPublisher(String publisher) {
+		this.Publisher = publisher;
+	}
+
+	public void setEditionYear(int edition) {
+		this.editionYear = edition;
+	}
+
+	public int getEditionYear() {
+		return this.editionYear;
+	}
+
+	public String getPublisher() {
+		return Publisher;
+	}
+
+}
