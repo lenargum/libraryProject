bba473f96b0114a1c3e76dbf769836bea30879d9
Lenar <lenargamgam@gmail.com>
2018-02-02 18:41:43 +0300

    database insertion example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
-	public static void main(String[] args) {
-//        LibrarianInterface librarian = new Librarian("A", "Adrs", "8999", 0);
-//        librarian.registerMaterial(new Book("A", "tLoR", 123,  true, 134));
-	}
+    public static void main(String[] args) throws Exception {
+        Database db = new Database();
+        db.connect();
+
+    }
 }
+/*
+db.execute("INSERT INTO users(login, password,status,firstname,lastname,phone,address)" +
+                " VALUES('lenargumerov','1234','admin','lenar','gumerov','2281337','unversitetskaya')");
+ */
