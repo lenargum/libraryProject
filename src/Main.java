25134d59db358c969cb62ff2ac77a6deb3a75914
Lenar <lenargamgam@gmail.com>
2018-02-02 17:38:15 +0300

    database connecting realization

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        Database db = new Database();
+        db.connect();
     }
 }
