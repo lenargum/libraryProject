9f4e21dba857332bbf9bf4f264f4484d83d523e1
Stanislaw Lem <stalem9@gmail.com>
2018-02-03 15:54:58 +0300

    Documentation in tools package

--- src/tools/TypeTesterIntarface.java
+++ src/tools/TypeTesterIntarface.java
@@ -6,17 +6,44 @@ import materials.JournalArticle;
 import users.Librarian;
 import users.Patron;
 
+/**
+ * interface describing class that checks type of current object
+ * (not bug, but feature)
+ */
 public interface TypeTesterIntarface {
-
+    /**
+     * checks type librarian
+     * @param librarian
+     */
     public void setType(Librarian librarian);
 
+    /**
+     * checks type of patron
+     * @param patron
+     */
     public void setType(Patron patron);
 
+    /**
+     * checks type book
+     * @param book
+     */
     public void setType(Book book);
 
+    /**
+     * check type journal article
+     * @param journalArticle
+     */
     public void setType(JournalArticle journalArticle);
 
+    /**
+     * checks type audio/video material
+     * @param audioVideoMaterial
+     */
     public void setType(AudioVideoMaterial audioVideoMaterial);
 
+    /**
+     * retruns type of current object
+     * @return
+     */
     public String getType();
 }
