9199c7a5da558c8a2746c80b3ef5839eb09a3e39
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-02-04 02:13:12 +0300

    Code reformat and core updates

---
+++ src/graphicalUI/NotAuthorizedException.java
@@ -0,0 +1,10 @@
+package graphicalUI;
+
+public class NotAuthorizedException extends Exception {
+	public NotAuthorizedException() {
+	}
+
+	public NotAuthorizedException(String message) {
+		super(message);
+	}
+}
