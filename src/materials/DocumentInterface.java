eb5a4a3fc7188e6aae031d5ac977c95860de5ab8
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-02-04 21:47:15 +0300

    Code improvements

--- src/materials/DocumentInterface.java
+++ src/materials/DocumentInterface.java
@@ -5,117 +5,119 @@ package materials;
  */
 public interface DocumentInterface {
 
-    /**
-     * sets title of current document
-     * @param Title
-     */
-    public void setTitle(String Title);
+	/**
+	 * @return title of current document
+	 */
+	public String getTitle();
 
-    /**
-     * sets author(s) names
-     * @param Authors
-     */
-    public void setAuthors(String Authors);
+	/**
+	 * sets title of current document
+	 *
+	 * @param Title
+	 */
+	public void setTitle(String Title);
 
-    /**
-     * sets Id of user who takes the document
-     * @param userID
-     */
-    public void setUserID(int userID);
+	/**
+	 * @return list of author of document
+	 */
+	public String getAuthors();
 
-    /**
-     * sets ID of current document
-     * @param id
-     */
-    public void setDocID(int id);
+	/**
+	 * sets author(s) names
+	 *
+	 * @param Authors
+	 */
+	public void setAuthors(String Authors);
 
-    /**
-     * sets price of current document
-     * @param price
-     */
-    public void setPrice(float price);
+	/**
+	 * @return price of current document
+	 */
+	public float getPrice();
 
-    /**
-     * sets status of this document - if it is already checked we cannot take it
-     * @param checked
-     */
-    public void setChecked(boolean checked);
+	/**
+	 * sets price of current document
+	 *
+	 * @param price
+	 */
+	public void setPrice(float price);
 
-    /**
-     * sets "function" of this document - if it is reference-book we cannot take it
-     * @param reference
-     */
-    public void setReference(boolean reference);
+	/**
+	 * @return ID of current document
+	 */
+	public int getDocID();
 
-    /**
-     * sets whether students can use this document
-     * @param allowedForStudents
-     */
-    public void setAllowedForStudents(boolean allowedForStudents);
+	/**
+	 * sets ID of current document
+	 *
+	 * @param id
+	 */
+	public void setDocID(int id);
 
-    /**
-     *
-     * @return title of current document
-     */
-    public String getTitle();
+	/**
+	 * @return ID of user who took Document
+	 */
+	public int getUserID();
 
-    /**
-     *
-     * @return list of author of document
-     */
-    public String getAuthors();
+	/**
+	 * sets Id of user who takes the document
+	 *
+	 * @param userID
+	 */
+	public void setUserID(int userID);
 
-    /**
-     *
-     * @return price of current document
-     */
-    public float getPrice();
+	/**
+	 * @return status of this document - if it is already checked we cannot take it
+	 */
+	public boolean isChecked(); //взяли книгу или нет
 
-    /**
-     *
-     * @return ID of current document
-     */
-    public int getDocID();
+	/**
+	 * sets status of this document - if it is already checked we cannot take it
+	 *
+	 * @param checked
+	 */
+	public void setChecked(boolean checked);
 
-    /**
-     *
-     * @return ID of user who took Document
-     */
-    public int getUserID();
+	/**
+	 * @return "function" of this document - if it is reference-book we cannot take it
+	 */
+	public boolean isReference(); //является ли документ справочным(влияет на возможность забрать его из библиотеки)
 
-    /**
-     *
-     * @return status of this document - if it is already checked we cannot take it
-     */
-    public boolean isChecked(); //взяли книгу или нет
+	/**
+	 * sets "function" of this document - if it is reference-book we cannot take it
+	 *
+	 * @param reference
+	 */
+	public void setReference(boolean reference);
 
-    /**
-     *
-     * @return "function" of this document - if it is reference-book we cannot take it
-     */
-    public boolean isReference(); //является ли документ справочным(влияет на возможность забрать его из библиотеки)
+	/**
+	 * @return whether students can use this document
+	 */
+	public boolean isAllowedForStudents();
 
-    /**
-     *
-     * @return whether students can use this document
-     */
-    public boolean isAllowedForStudents();
+	/**
+	 * sets whether students can use this document
+	 *
+	 * @param allowedForStudents
+	 */
+	public void setAllowedForStudents(boolean allowedForStudents);
 
 //    public boolean canTake(Patron user);
 
 //    public boolean isFaculty(Patron x);
 
-    /**
-     * if librarian finds out that there are one more author of this document he can add him
-     * @param newAuthor
-     */
-    public void addAuthor(String newAuthor);
+	/**
+	 * if librarian finds out that there are one more author of this document he can add him
+	 *
+	 * @param newAuthor
+	 */
+	public void addAuthor(String newAuthor);
 
-    /**
-     * sometimes we need to check whether on person is author of some document
-     * @param author
-     * @return whether the person is author of current document
-     */
-    public boolean isWrittenBy(String author);
+	/**
+	 * sometimes we need to check whether on person is author of some document
+	 *
+	 * @param author
+	 * @return whether the person is author of current document
+	 */
+	public boolean isWrittenBy(String author);
 
 }
