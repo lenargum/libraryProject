bb42529fc62e266b6f9c58bb46ff232f1c1b007d
Madina Gafarova <34398145+gafmn@users.noreply.github.com>
2018-02-04 17:50:31 +0300

    Now Document is abstract class.

--- src/materials/DocumentInterface.java
+++ src/materials/DocumentInterface.java
@@ -1,146 +1,44 @@
 package materials;
 
-/**
- * Interface describes all the documents as class
- */
 public interface DocumentInterface {
+    void setTitle(String Title);
 
-	/**
-	 * returns title of document
-	 *
-	 * @return
-	 */
-	public String getTitle();
+    void setAuthors(String Authors);
 
-	/**
-	 * sets title of document
-	 *
-	 * @param Title
-	 */
-	public void setTitle(String Title);
+    void setUserID(int userID);
 
-	/**
-	 * returns name or list of names of author(s) the document was created by
-	 *
-	 * @return
-	 */
-	public String getAuthors();
+    void setDocID(int id);
 
-	/**
-	 * sets name or list of names of author(s) the document was created by
-	 *
-	 * @param Authors
-	 */
-	public void setAuthors(String Authors);
+    void setPrice(float price);
 
-	/**
-	 * returns price of current document
-	 *
-	 * @return
-	 */
-	public float getPrice();
+    void setChecked(boolean checked);
 
-	/**
-	 * sets price of current document
-	 *
-	 * @param price
-	 */
-	public void setPrice(float price);
+    void setReference(boolean reference);
 
-	/**
-	 * returns ID of current document
-	 *
-	 * @return
-	 */
-	public int getDocID();
+    void setAllowedForStudents(boolean allowedForStudents);
 
-	/**
-	 * sets ID of current document
-	 *
-	 * @param id
-	 */
-	public void setDocID(int id);
+    String getTitle();
 
-	/**
-	 * returns ID of user the Document was taken by
-	 *
-	 * @return
-	 */
-	public int getUserID();
+    String getAuthors();
 
-	/**
-	 * sets ID of user the Document was taken by
-	 *
-	 * @param userID
-	 */
-	public void setUserID(int userID);
+    float getPrice();
 
-	/**
-	 * returns current state of document: true if it is taken by some user, false otherwise
-	 *
-	 * @return
-	 */
-	public boolean isChecked(); //взяли книгу или нет
+    int getDocID();
 
-	/**
-	 * sets current state of document: true if it is taken by some user, false otherwise
-	 *
-	 * @param checked
-	 */
-	public void setChecked(boolean checked);
+    int getUserID();
 
-	/**
-	 * returns value of current document: true if document is reference-book and false otherwise
-	 *
-	 * @return
-	 */
-	public boolean isReference(); //является ли документ справочным(влияет на возможность забрать его из библиотеки)
+    boolean isChecked(); //взяли книгу или нет
 
-	/**
-	 * sets value of current document: true if document is reference-book and false otherwise
-	 *
-	 * @param reference
-	 */
-	public void setReference(boolean reference);
+    boolean isReference(); //является ли документ справочным(влияет на возможность забрать его из библиотеки)
 
-	/**
-	 * returns parametr which shows possibility for students to take the document
-	 *
-	 * @return
-	 */
-	public boolean isAllowedForStudents();
-
-	/**
-	 * sets parametr which shows possibility for students to take the document
-	 *
-	 * @param allowedForStudents
-	 */
-	public void setAllowedForStudents(boolean allowedForStudents);
+    boolean isAllowedForStudents();
 
 //    public boolean canTake(Patron user);
 
 //    public boolean isFaculty(Patron x);
 
-	/**
-	 * adds Author to the list of authors, the document was created by
-	 *
-	 * @param newAuthor is author that need to be added
-	 */
-	public void addAuthor(String newAuthor);
+    void addAuthor(String newAuthor);
 
-	/**
-	 * checks if author is among list of persons the document was created by
-	 *
-	 * @param author
-	 * @return true if author toook a part in document creation
-	 */
-	public boolean isWrittenBy(String author);
-
-    /**
-     * checks wether one document is copy of another
-     * @param other
-     * @return true if current document is copy of other
-     */
-    public boolean equals(Document other);
+    boolean isWrittenBy(String author);
 
 }
--- src/users/Patron.java
+++ src/users/Patron.java
@@ -5,80 +5,80 @@ import materials.Document;
 import java.util.LinkedList;
 
 public class Patron extends User implements PatronInterface {
-	private String status;
-	private LinkedList<Document> listOfDocumentsPatron = new LinkedList<>();
-	private int debts;
+    private String status;
+    private LinkedList<Document> listOfDocumentsPatron = new LinkedList<>();
+    private int debts = 0;
 
 
-	public Patron(String name, String address, String phoneNumber, String status, int id) {
-		super(name, address, phoneNumber, id);
-		this.status = status;
-	}
+    Patron(String name, String address, String phoneNumber, String status, int id) {
+        super(name, address, phoneNumber, id);
+        this.status = status;
+    }
 
-	@Override
-	public String getStatus() {
-		return status;
-	}
+    @Override
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	@Override
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    @Override
+    public String getStatus() {
+        return status;
+    }
 
-	@Override
-	public LinkedList<Document> getListOfDocumentsPatron() {
-		return listOfDocumentsPatron;
-	}
+    @Override
+    public LinkedList<Document> getListOfDocumentsPatron() {
+        return listOfDocumentsPatron;
+    }
 
-	@Override
-	public int getDebts() {
-		return debts;
-	}
+    @Override
+    public void setDebts(int debts) {
+        this.debts = debts;
+    }
 
-	@Override
-	public void setDebts(int debts) {
-		this.debts = debts;
-	}
+    @Override
+    public int getDebts() {
+        return debts;
+    }
 
-	@Override
-	public void addDocumentInList(Document document) {
-		listOfDocumentsPatron.add(document);
-	}
+    @Override
+    public void addDocumentInList(Document document) {
+        listOfDocumentsPatron.add(document);
+    }
 
-	@Override
-	public boolean getRequest(int idDocument, Librarian librarian) {
-		//TODO: Request to take a book from the library
-		if (this.getStatus() == "faculty") {
-			return true;
-		} else {
-			if (librarian.manager.listOfDocuments.get(idDocument).isAllowedForStudents())
-				return true;
-			else
-				return false;
-		}
-	}
+    @Override
+    public boolean getRequest(int idDocument, Librarian librarian) {
+        //TODO: Request to take a book from the library
+        if (this.getStatus() == "faculty") {
+            return true;
+        } else {
+            if (librarian.manager.listOfDocuments.get(idDocument).isAllowedForStudents())
+                return true;
+            else
+                return false;
+        }
+    }
 
-	@Override
-	public void takeDocument(int idDocument, Librarian librarian) {
-		if (getRequest(idDocument, librarian)) {
-			this.addDocumentInList(librarian.manager.listOfDocuments.get(idDocument));
-			librarian.manager.listOfDocuments.get(idDocument).setChecked(true);
-			librarian.manager.listOfDocuments.get(idDocument).setUserID(this.getId());
-		} else {
-			System.out.println("Access is not available");
-		}
-	}
+    @Override
+    public void takeDocument(int idDocument, Librarian librarian) {
+        if (getRequest(idDocument, librarian)) {
+            this.addDocumentInList(librarian.manager.listOfDocuments.get(idDocument));
+            librarian.manager.listOfDocuments.get(idDocument).setChecked(true);
+            librarian.manager.listOfDocuments.get(idDocument).setUserID(this.getId());
+        } else {
+            System.out.println("Access is not available");
+        }
+    }
 
-	@Override
-	public LinkedList<Document> getDocumentsInLibrary(Librarian librarian) {
-		return librarian.manager.listOfDocuments;
-	}
+    @Override
+    public LinkedList<Document> getDocumentsInLibrary(Librarian librarian) {
+        return librarian.manager.listOfDocuments;
+    }
 
 
-	public void bringBackDocument(int idDocument, Librarian librarian) {
-		this.getListOfDocumentsPatron().remove(librarian.manager.listOfDocuments.get(idDocument));
-		librarian.manager.listOfDocuments.get(idDocument).setChecked(false);
-		librarian.manager.listOfDocuments.get(idDocument).setUserID(-1);
-		//TODO: set/get who took document
-	}
+    public void bringBackDocument(int idDocument, Librarian librarian) {
+        this.getListOfDocumentsPatron().remove(librarian.manager.listOfDocuments.get(idDocument));
+        librarian.manager.listOfDocuments.get(idDocument).setChecked(false);
+        librarian.manager.listOfDocuments.get(idDocument).setUserID(-1);
+        //TODO: set/get who took document
+    }
 }
