3f71dd46d5b399f2e298a828a1c811254971e2bd
Stanislaw Lem <stalem9@gmail.com>
2018-02-05 10:04:10 +0300

    Testing TypeTester in tools package

---
+++ Tests/tools/SetManagerTest.java
@@ -0,0 +1,24 @@
+package tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetManagerTest {
+    @Test
+    void addPatron() {
+    }
+
+    @Test
+    void deletePatron() {
+    }
+
+    @Test
+    void addDocumentInLibrary() {
+    }
+
+    @Test
+    void deleteDocumentFromLibrary() {
+    }
+
+}
