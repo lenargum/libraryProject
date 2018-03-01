cccc4e67289510deab21a04821a3fb3f4079b816
Lenar <lenargamgam@gmail.com>
2018-03-02 00:56:01 +0300

    deleting and editing features

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+
+
+        //example of database usage
         Database db = new Database();
         db.connect();
         if (db.isConnected()) {
-            /*db.insertArticle("testing data", "Lenar Gumerov", true, 0,
-                    false, 0.5, "hate date","databases","somebody",
-                    "dafsafs","asdfasdf","2017-01-01");
-            */
-            //////////testing/////////////
-            ArrayList<String> shit = db.getDocumentList();
-            for(String temp : shit) {
-                System.out.println(temp);
-            }
-            ArrayList<AudioVideoMaterial> ya = db.getAVList();
-            ArrayList<JournalArticle> zaebalsya = db.getArticleList();
+            //body of database interaction
+
+
+
+            //not necessarily, but desirable
             db.close();
         }
     }
 }
-/*
-db.execute("INSERT INTO users(login, password,status,firstname,lastname,phone,address)" +
-                " VALUES('lenargumerov','1234','admin','lenar','gumerov','2281337','unversitetskaya')");
- */
