600deb958d60a80c2967ad039e5a5bfd729290f1
Stanislaw Lem <stalem9@gmail.com>
2018-03-07 14:41:10 +0300

    added function get number of copies

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,3 +1,4 @@
+import javax.print.Doc;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
@@ -146,6 +147,15 @@ public class Librarian extends User {
 		} catch (SQLException e) {
 			System.out.println("Incorrect input");
 		}
+	}
+
+	public int getNumberOfDocument(Database database)throws SQLException{
+		int n = 0;
+		for (Document i:database.getDocumentList()
+			 ) {
+			n += i.getNumberOfCopies();
+		}
+		return n;
 	}
 
 }
